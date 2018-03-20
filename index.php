d722c61ec91a0b97653baf5453bc46512c2a8bf4
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2018-03-21 03:07:40 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,20 @@
     function br() {
         echo nl2br("\n");
     }
-    function unicode_encode($str) {
-        return preg_replace_callback("/\\\\u([0-9a-zA-Z]{4})/", "encode_callback", $str);
+    function json_xencode($value, $options = 0, $unescapee_unicode = true)
+    {
+      $v = json_encode($value, $options);
+      if ($unescapee_unicode) {
+        $v = unicode_encode($v);
+        $v = preg_replace('/\\\\\//', '/', $v);
+      }
+      return $v;
     }
     define (URL, (empty($_SERVER['HTTPS']) ? 'http://' : 'https://').$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-    $encoded = unicode_encode($_GET["unicode"]);
+    
     if (strlen($_GET["unicode"]) >= 1){
         $json_array = array(
-            'conv' => "$encoded",
+            'conv' => json_xencode($_GET["unicode"]),
         );
         header("Content-Type: text/javascript; charset=utf-8");
         echo json_encode($json_array);
