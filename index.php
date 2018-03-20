e466d5929fc09d795d1c3a6b461a359e363aad67
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2018-03-21 03:00:27 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         return preg_replace_callback("/\\\\u([0-9a-zA-Z]{4})/", "encode_callback", $str);
     }
     define (URL, (empty($_SERVER['HTTPS']) ? 'http://' : 'https://').$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-    $encoded = unicode_encode($decoded);
+    $encoded = unicode_encode($_GET["unicode"]);
     if (strlen($_GET["unicode"]) >= 1){
         $json_array = array(
             'conv' => "$encoded",
