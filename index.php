3e968fa62a86bdfbbb056074229405ca94440289
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2018-03-21 03:11:35 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,20 +2,23 @@
     function br() {
         echo nl2br("\n");
     }
-    function json_xencode($value, $options = 0, $unescapee_unicode = true)
-    {
-      $v = json_encode($value, $options);
-      if ($unescapee_unicode) {
-        $v = unicode_encode($v);
-        $v = preg_replace('/\\\\\//', '/', $v);
-      }
-      return $v;
+    function unicode_unescape($str) {
+      $callback = function ($matches) {
+        if (empty($matches[2])) {
+          $code = hexdec($matches[3]);
+          return mb_convert_encoding(pack("N*", $code), "UTF-8", "UTF-32BE");
+        } else {
+          $code = hexdec($matches[2]);
+          return mb_convert_encoding(pack("n*", $code), "UTF-8", "UTF-16BE");
+        }
+      };
+      return preg_replace_callback("/(\\\\u([0-9a-zA-Z]{4})|\\\\U([0-9a-zA-Z]{8}))/", $callback, $str);
     }
     define (URL, (empty($_SERVER['HTTPS']) ? 'http://' : 'https://').$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
     
     if (strlen($_GET["unicode"]) >= 1){
         $json_array = array(
-            'conv' => json_xencode($_GET["unicode"]),
+            'conv' => unicode_unescape($_GET["unicode"]),
         );
         header("Content-Type: text/javascript; charset=utf-8");
         echo json_encode($json_array);
