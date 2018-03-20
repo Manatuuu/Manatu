44c5676ecd75ccae44e86444f44fbbb1bfa7da23
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2018-03-21 04:06:27 +0900

    Update and rename a to unicode-api.php

---
+++ API/unicode-api.php
@@ -0,0 +1,23 @@
+<?php
+    function br() {
+        echo nl2br("\n");
+    }
+    function unicode_sequence_decode($string)
+    {
+        return json_decode('"'.$string.'"');
+    }
+    function raw_json_encode($array, $flags = 0) {
+        return preg_replace_callback('/\\\\u([0-9a-zA-Z]{4})/', function ($m) {
+            return html_entity_decode("&#x$m[1];", 0, 'UTF-8');
+        }, json_encode($array, $flags));
+    }
+    if (strlen($_GET["unicode"]) >= 1){
+        $array = array('conv' => unicode_sequence_decode($_GET["unicode"]));
+        $raw_json = raw_json_encode($array);
+        echo $raw_json;
+        return;
+    }
+    $array = array('conv' => 'エラー: unicodeがセットされていません');
+    $raw_json = raw_json_encode($array);
+    echo $raw_json;
+?>
