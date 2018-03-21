4abe5dd085c20e5c3d70ace565ddab62451544b2
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2018-03-21 12:53:26 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
+function init_background_process(){
+    ob_end_clean();
+    header("Connection: close\r\n");
+    header("Content-Encoding: none\r\n");
+    ignore_user_abort(true);
+    ob_start();
+}
 
+function start_background_process(){
+    $size = ob_get_length();
+    header("Content-Length: $size");
+    ob_end_flush();
+    flush();
+    ob_end_clean();
+}
 
->
+init_background_process();
+
+echo 'test';
+
+start_background_process();
+?>
