fcf68926732eb9f18818fcceb72543cbe2612b5f
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2018-03-21 13:48:38 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 
 <?php
   $url = "https://api.hypixel.net/player?key=12755d3c-51c6-4926-bb41-2baeb72d4c0c&name=skquery";
-  $conn = curl_init();
-  curl_setopt($conn, CURLOPT_URL, $url);
-  curl_setopt($conn, CURLOPT_RETURNTRANSFER, true);
-  $res =  curl_exec($conn);
-  var_dump($res);
-  curl_close($conn);
+  require_once("./phpQuery-onefile.php");
+  $html = file_get_contents("https://ja.wikipedia.org/wiki/%E3%82%A6%E3%82%A7%E3%83%96%E3%82%B9%E3%82%AF%E3%83%AC%E3%82%A4%E3%83%94%E3%83%B3%E3%82%B0");
+  echo phpQuery::newDocument($html)->find("h1")->text();
 ?>
