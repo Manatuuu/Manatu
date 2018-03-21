b84ed97bfc3f5e06e607d4a51fc871ff7ca0ce51
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2018-03-21 18:01:36 +0900

    Update index.php

--- HELP/index.php
+++ HELP/index.php
@@ -4,18 +4,18 @@
       <meta charset="utf-8">
       <title>API List</title>
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-      <link rel="stylesheet" href="assets/css/main.css">
-      <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-      <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-      <link rel="shortcut icon" href="assets/images/favicon-32x32.ico" type="image/x-icon"/>
-      <link rel="shortcut icon" href="assets/images/favicon-16x16.ico" type="image/x-icon"/>
+      <!--[if lte IE 8]><script src="../assets/js/ie/html5shiv.js"></script><![endif]-->
+      <link rel="stylesheet" href="../assets/css/main.css">
+      <!--[if lte IE 8]><link rel="stylesheet" href="../assets/css/ie8.css" /><![endif]-->
+      <!--[if lte IE 9]><link rel="stylesheet" href="../assets/css/ie9.css" /><![endif]-->
+      <link rel="shortcut icon" href="../assets/images/favicon-32x32.ico" type="image/x-icon"/>
+      <link rel="shortcut icon" href="../assets/images/favicon-16x16.ico" type="image/x-icon"/>
     </head>
     <body>
     <div class="container">
       <div class="inner">
         <div class="six column" style="margin-top: 5em">
-        <img src="assets/images/logo.png">
+        <img src="../assets/images/logo.png">
         <h2>API List</h2>
           <span>
             <button onclick="window.location.href='https://stark-reaches-14922.herokuapp.com/API'">API List</button>
