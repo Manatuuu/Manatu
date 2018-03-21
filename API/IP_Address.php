dde9cf7249259557930892c5605b5a6eb8908362
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2018-03-21 18:31:42 +0900

    Update IP_Address.php

--- API/IP_Address.php
+++ API/IP_Address.php
@@ -1,1 +1,10 @@
-hello
+<?php
+    function raw_json_encode($array, $flags = 0) {
+        return preg_replace_callback('/\\\\u([0-9a-zA-Z]{4})/', function ($m) {
+            return html_entity_decode("&#x$m[1];", 0, 'UTF-8');
+        }, json_encode($array, $flags));
+    }
+    $array = array('ip' => '$_SERVER['HTTP_X_FORWARDED_FOR']');
+    $raw_json = raw_json_encode($array);
+    echo $raw_json;
+?>
