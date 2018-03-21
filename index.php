067da3cb53e13ce94d05021099478429f1d112db
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2018-03-21 15:14:51 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,32 @@
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>API List</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-<link rel="stylesheet" href="assets/css/main.css">
-<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-<link rel="shortcut icon" href="assets/images/favicon-32x32.ico" type="image/x-icon"/>
-<link rel="shortcut icon" href="assets/images/favicon-16x16.ico" type="image/x-icon"/>
-</head>
-<body>
-<div class="container">
-<div class="center">
-<div class="six column" style="margin-top: 5em">
-<img src="assets/images/logo.png">
-<h2>My API List</h2>
-<span>
-<ul class="actions">
-<button onclick="window.location.href='https://stark-reaches-14922.herokuapp.com/API'">API List</button>
-<button onclick="window.location.href='https://stark-reaches-14922.herokuapp.com/HELP'">Help</button>
-</ul>
-</span>
-</div>
-</div>
-</div>
-</body>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>API List</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+      <link rel="stylesheet" href="assets/css/main.css">
+      <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+      <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+      <link rel="shortcut icon" href="assets/images/favicon-32x32.ico" type="image/x-icon"/>
+      <link rel="shortcut icon" href="assets/images/favicon-16x16.ico" type="image/x-icon"/>
+    </head>
+    <body>
+    <div class="container">
+      <div class="center">
+        <div class="six column" style="margin-top: 5em">
+        <img src="assets/images/logo.png">
+        <h2>My API List</h2>
+          <span>
+            <ul class="actions">
+              <li><a href="#one" class="button scrolly">Discover</a></li>
+              <button onclick="window.location.href='https://stark-reaches-14922.herokuapp.com/API'">API List</button>
+              <button onclick="window.location.href='https://stark-reaches-14922.herokuapp.com/HELP'">Help</button>
+            </ul>
+          </span>
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
