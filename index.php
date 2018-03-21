d551a146566698e4c8a1c8e4fc715cea3149ae47
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2018-03-21 13:44:08 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,10 @@
-<style type="text/css">
-#d_background1 {
-	background:#ff6699 url(BGAwCbV-.jpg) repeat 0 0;
-	padding:20px;
-	color:#fff;
-	font-weight:bold;
-	text-align:center;
-}
-</style>
 
-<div id="d_background1">
-	background:#ff6699 url(BGAwCbV-.jpg) repeat 0 0;
-</div>
 <?php
-function init_background_process(){
-    ob_end_clean();
-    header("Connection: close\r\n");
-    header("Content-Encoding: none\r\n");
-    ignore_user_abort(true);
-    ob_start();
-}
-
-function start_background_process(){
-    $size = ob_get_length();
-    header("Content-Length: $size");
-    ob_end_flush();
-    flush();
-    ob_end_clean();
-}
-
-init_background_process();
-
-echo 'test';
-
-start_background_process();
+  $url = "https://api.hypixel.net/player?key=12755d3c-51c6-4926-bb41-2baeb72d4c0c&name=skquery";
+  $conn = curl_init();
+  curl_setopt($conn, CURLOPT_URL, $url);
+  curl_setopt($conn, CURLOPT_RETURNTRANSFER, true);
+  $res =  curl_exec($conn);
+  var_dump($res);
+  curl_close($conn);
 ?>
