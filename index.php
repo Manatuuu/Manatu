2a417ee448333acc77555440e700b8c8b861e210
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2018-03-21 15:07:00 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
 <meta charset="utf-8">
 <title>API List</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 <link rel="stylesheet" href="assets/css/main.css">
+<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
 <link rel="shortcut icon" href="assets/images/favicon-32x32.ico" type="image/x-icon"/>
 <link rel="shortcut icon" href="assets/images/favicon-16x16.ico" type="image/x-icon"/>
 </head>
