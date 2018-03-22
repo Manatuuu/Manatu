0aa41309c6dc6eb3aa52820961f7d3e13df649fc
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2018-03-22 13:11:33 +0900

    Create 404.php

---
+++ API/404.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <link rel="stylesheet" href="404.css">
+    </head>
+</html>
+<div id="clouds">
+            <div class="cloud x1"></div>
+            <div class="cloud x1_5"></div>
+            <div class="cloud x2"></div>
+            <div class="cloud x3"></div>
+            <div class="cloud x4"></div>
+            <div class="cloud x5"></div>
+        </div>
+        <div class='c'>
+            <div class='_404'>404</div>
+            <hr>
+            <div class='_2'>このページには何も存在しません</div>
+            <a class='btn' href='https://stark-reaches-14922.herokuapp.com/API'>戻る</a>
+        </div>
