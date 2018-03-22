614ae6a3b4a98063e787c9940e648bc5381b56af
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2018-03-22 15:09:31 +0900

    Update index.php

--- test/index.php
+++ test/index.php
@@ -1,5 +1,5 @@
 <?php
   require_once("./phpQuery-onefile.php");
-  $name = '$_GET["name"]';
+  $name = $_GET["name"];
   echo "https://api.hypixel.net/player?key=12755d3c-51c6-4926-bb41-2baeb72d4c0c&name=$name";
 ?>
