85a1fe4ada17de02165c60b6d6750b0226f51973
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2018-03-22 15:00:23 +0900

    Update index.php

--- test/index.php
+++ test/index.php
@@ -1,7 +1,7 @@
 <?php
   require_once("./phpQuery-onefile.php");
-  $html = file_get_contents('https://api.hypixel.net/player?key=12755d3c-51c6-4926-bb41-2baeb72d4c0c&name=$_GET["name"]');
+  $url = 'https://api.hypixel.net/player?key=12755d3c-51c6-4926-bb41-2baeb72d4c0c&name=$_GET["name"]'
+  $html = file_get_contents($url);
   header("Content-Type: text/javascript; charset=utf-8");
-  echo $_GET["name"]
   echo phpQuery::newDocument($html)->text();
 ?>
